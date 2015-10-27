29a1c4c08ac79efd6e1f860e40a483f2b1f8db9a
nanhee <nin@fam13.sinc.stonybrook.edu>
2015-10-27 10:41:26 -0400

    hh

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 var r, g, b;
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(700, 600);
   // Pick colors randomly
   r = random(255);
   g = random(255);
@@ -46,6 +46,42 @@ function draw() {
   stroke(r, g, b);
   fill(r, g, b, 127);
   ellipse(600, 300, 150, 150);
+  
+  // Draw a circle
+  strokeWeight(2);
+  stroke(r, g, b);
+  fill(r, g, b, 127);
+  ellipse(150, 500, 150, 150);
+
+  // Draw a circle
+  strokeWeight(2);
+  stroke(r, g, b);
+  fill(r, g, b, 127);
+  ellipse(370, 500, 150, 150);
+
+  // Draw a circle
+  strokeWeight(2);
+  stroke(r, g, b);
+  fill(r, g, b, 127);
+  ellipse(600, 500, 150, 150);
+
+  // Draw a circle
+  strokeWeight(2);
+  stroke(r, g, b);
+  fill(r, g, b, 127);
+  ellipse(150, 700, 150, 150);
+
+  // Draw a circle
+  strokeWeight(2);
+  stroke(r, g, b);
+  fill(r, g, b, 127);
+  ellipse(370, 700, 150, 150);
+
+  // Draw a circle
+  strokeWeight(2);
+  stroke(r, g, b);
+  fill(r, g, b, 127);
+  ellipse(600, 700, 150, 150);
 }
 // When the user clicks the mouse
 function mousePressed() {
@@ -53,52 +89,52 @@ function mousePressed() {
   var d = dist(mouseX, mouseY, 150, 100);
   if (d < 50) {
     // Pick new random color values
-    r = random(204);
-    g = random(255);
-    b = random(255);
+    r = (204);
+    g = (255);
+    b = (255);
 }
   // Check if mouse is inside the circle
   var q = dist(mouseX, mouseY, 400, 100);
   if (q < 50) {
     // Pick new random color values
-    r = random(255);
-    g = random(204);
-    b = random(204);
+    r = (255);
+    g = (204);
+    b = (204);
   }
   
   // Check if mouse is inside the circle
   var w = dist(mouseX, mouseY, 600, 100);
   if (w < 50) {
     // Pick new random color values
-    r = random(204);
-    g = random(255);
-    b = random(255);
+    r = (204);
+    g = (255);
+    b = (255);
   }
   
   // Check if mouse is inside the circle
   var e = dist(mouseX, mouseY, 150, 300);
   if (e < 50) {
     // Pick new random color values
-    r = random(255);
-    g = random(255);
-    b = random(204);
+    r = (255);
+    g = (255);
+    b = (204);
   }
   
   // Check if mouse is inside the circle
   var t = dist(mouseX, mouseY, 400, 300);
   if (t < 150) {
     // Pick new random color values
-    r = random(255);
-    g = random(204);
-    b = random(204);
+    r = (255);
+    g = (204);
+    b = (204);
   }
   
   // Check if mouse is inside the circle
   var y = dist(mouseX, mouseY, 600, 300);
   if (y < 150) {
     // Pick new random color values
-    r = random(204);
-    g = random(255);
-    b = random(204);
+    r = (204);
+    g = (255);
+    b = (204);
   }
 }
